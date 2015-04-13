050662d9cae46caba4620ada2e7f6ac90071103d
swarnez <sebastian@adorablemonkeygames.com>
2015-04-13 12:34:41 +0200

    Show league info when clicking season
    - Seasons and standings moved to reactive collections
    - no CSS yet

--- adorablemonkey.js
+++ adorablemonkey.js
@@ -1,68 +1,119 @@
+Seasons = new Meteor.Collection('seasons');
+Standings = new Meteor.Collection('standings');
+var defaultSeason = 351;
+
 //------------- CLIENT -------------
 if (Meteor.isClient) {  
-  var seasons = null;
-  var seasonsDep = new Deps.Dependency();
-
-  Template.hudl.rendered = function() {
-    Meteor.call('getSeasons', function(error, result) {
-      if (!error) {
-        seasons = JSON.parse(result);
-
-        //Json doesn't contain id, so extract from link
-        $.each(seasons, function(index, value) {
-          var seasonId = value._links.self.href.split("http://api.football-data.org/alpha/soccerseasons/")[1];
-          value["id"] = seasonId;
-        });
-
-        seasonsDep.changed();
-      }
-    });
-  }
+  Session.setDefault("leagueCaption", "");
 
   Template.hudl.helpers({
 
     seasons: function() {
-      seasonsDep.depend();
-      return seasons;
+      return Seasons.find({});
+    },
+
+    leagueCaption: function() {
+      return Session.get("leagueCaption");
+    },
+
+    standings: function() {
+      return Standings.find({});
     }
+
+  });
+
+  Template.season.events({
+    "click .showInfo": function () {      
+      Session.set("leagueCaption", this.caption);
+      Meteor.call('updateLeagueTable', this.id);
+    }
+
   });
 }
 
 //------------- SERVER -------------
 if (Meteor.isServer) {
   var AUTH_TOKEN = "0dc2b4843a2147f586b8b477af21d841";
+  var baseUrl = "http://api.football-data.org/alpha/soccerseasons/";
 
   Meteor.startup(function() {
-    
+
+    Meteor.call('getSeasons', function(error, result) {
+      if (!error) {
+        var seasons = JSON.parse(result);
+        seasons.forEach(function(season, i, v) {          
+          //Json doesn't contain id, so extract from link
+          var seasonId = season._links.self.href.split(baseUrl)[1];
+
+          Seasons.upsert({ 
+            id: seasonId
+          }, {
+            $set: {
+              caption: season['caption'],
+              league: season['league'],
+              year: season['year'],
+              numberOfTeams: season['numberOfTeams'],
+              numberOfGames: season['numberOfGames'],
+              lastUpdated: season['lastUpdated']
+            }
+          });
+        });
+      }
+    });
+
   });
 
   Meteor.methods({
-    
-    getSeasons: function () {
-      try
-      {
-        //TODO: Put in reusable function that passes url
-        var result = Meteor.http.get("http://api.football-data.org/alpha/soccerseasons", {
-           headers: {
-              "X-Auth-Token": AUTH_TOKEN
-           } 
+
+    getSeasons: function() {      
+      return Meteor.call('getData', baseUrl);
+    },
+
+    updateLeagueTable: function(seasonId) {
+      Standings.remove({});
+
+      //NOTE: We could have fetched all standings on startup and store locally by season,
+      //but let's simulate that the data changes often for funsies.
+      Meteor.call('getLeagueTable', seasonId, function(error, result) {
+        if (!error) {
+          var leagueTable = JSON.parse(result);        
+          var standings = leagueTable['standing'];
+
+          standings.forEach(function(standing, i, v) {
+            Standings.insert({
+              position: standing['position'],
+              teamName: standing['teamName'],
+              playedGames: standing['playedGames'],
+              points: standing['points'],
+              goals: standing['goals'],
+              goalsAgainst: standing['goalsAgainst'],
+              goalDifference: standing['goalDifference'],
+            });
+          });
+        }
+      });
+    },
+
+    getLeagueTable: function (seasonId) {
+      return Meteor.call('getData', baseUrl + seasonId + "/leagueTable");
+    },
+
+    getData: function(url)
+    {
+      try {
+        var result = Meteor.http.get(url, {
+          headers: {
+            "X-Auth-Token": AUTH_TOKEN
+          } 
         });
 
-        if (result.statusCode == 200)
-        {
+        if (result.statusCode == 200) {
           return result.content;
         }
       }
-      catch (e)
-      {
+      catch (e) {
         console.log("Error: " + e);
       }
-    },
-
-    getLeagueTable: function (leagueId) {
-      //console.log("GET LEAGUE: " + leagueId);
-
-      return leagueId;
     }  
 
   });
@@ -72,11 +123,4 @@ if (Meteor.isServer) {
 Router.map(function(){
     this.route('main', {path: '/'} );
     this.route('hudl', {path: '/hudl'});
-    
-    this.route('league', {
-      path: '/hudl/league/:seasonId',
-      data: function() {
-        Meteor.call('getLeagueTable', this.params.seasonId);
-      }
-    });
 });
